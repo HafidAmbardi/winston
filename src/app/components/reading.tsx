1800dfec5870a4855ecb7d2b4b50793cb5ef41d0
Nareswari Ayu Prabowo <144470962+Narscode@users.noreply.github.com>
2025-03-31 21:44:04 +0700

    Create reading.tsx

---
+++ src/app/components/reading.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ReadingSection = () => {
+  return (
+    <div className="w-full h-full bg-white rounded-2xl border border-gray-600 p-6 relative">
+      <div className="text-black text-sm font-normal leading-[21px]">Level Pemula</div>
+      <div className="w-full border-t border-gray-300 my-4"></div>
+      <div className="text-center text-black text-2xl font-semibold leading-[35px] px-4">
+        Peluang Karir atau Pekerjaan Sebagai Perempuan di Bidang Sains dan Teknologi
+      </div>
+      <div className="flex justify-center mt-6">
+        <img 
+          src="" 
+          alt="Reading Section" 
+          className="w-[435px] h-[199px] rounded-md"/>
+      </div>
+      <div className="text-justify text-black text-sm font-normal leading-[21px] px-6 mt-8">
+        Keterampilan STEM (Sains, Teknologi, Teknik, dan Matematika) telah dipandang sebagai sebuah keharusan bagi negara
+        yang ingin tetap bersaing di bidang ekonomi. Para ahli telah mengemukakan bahwa kualitas masyarakat secara
+        keseluruhan akan meningkat saat berbagai tim dapat mengatasi masalah teknologi dan sains. Sayangnya, tingkat
+        keterwakilan kaum perempuan di bidang STEM terus mengalami penurunan, dan kesenjangan ini bermula di kelas
+        perguruan tinggi. Namun, kesenjangan ini juga memiliki dasar sejarah.
+      </div>
+      <div className="text-justify text-black text-sm font-normal leading-[21px] px-6 mt-6">
+        Keterampilan STEM (Sains, Teknologi, Teknik, dan Matematika) telah dipandang sebagai sebuah keharusan bagi negara
+        yang ingin tetap bersaing di bidang ekonomi. Para ahli telah mengemukakan bahwa kualitas masyarakat secara
+        keseluruhan akan meningkat saat berbagai tim dapat mengatasi masalah teknologi dan sains. Sayangnya, tingkat
+        keterwakilan kaum perempuan di bidang STEM terus mengalami penurunan, dan kesenjangan ini bermula di kelas
+        perguruan tinggi. Namun, kesenjangan ini juga memiliki dasar sejarah.
+      </div>
+    </div>
+  );
+};
+
+export default ReadingSection;
