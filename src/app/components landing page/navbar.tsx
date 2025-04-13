2d87246e2b9ae3c56e3ac74ad004a39548f08085
Nareswari Ayu Prabowo <144470962+Narscode@users.noreply.github.com>
2025-04-13 10:57:48 +0700

    Update navbar.tsx

--- src/app/components landing page/navbar.tsx
+++ src/app/components landing page/navbar.tsx
@@ -18,20 +18,20 @@ const NavLink = ({ href, children, className }: NavLinkProps) => (
 export default function Navbar() {
   return (
     <nav className="w-full px-6 py-4 bg-white shadow-sm border-b border-black/10 flex justify-between items-center">
-      {/* Logo and Brand Name */}
+
       <div className="flex items-center gap-3">
         <Image src="/images/winston_logo.png" alt="Winston Logo" width={63} height={45} className="object-contain" />
         <span className="text-xl font-semibold font-['Plus_Jakarta_Sans'] text-black">Winston</span>
       </div>
 
-      {/* Navigation Links */}
+
       <div className="flex items-center justify-between gap-8">
         <NavLink href="/tentang-kami">Tentang Kami</NavLink>
         <NavLink href="/notes">Notes</NavLink>
         <NavLink href="/latihan-soal">Latihan soal</NavLink>
         <NavLink href="/package">Package</NavLink>
 
-        {/* Buttons */}
+
         <div className="flex items-center gap-2 ml-8">
           <Link
             href="/login"
